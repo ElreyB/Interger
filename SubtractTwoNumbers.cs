6a6bddf137648c7ee24f4cea487ce94feb816b01
ElreyB <elbelmonti@gmail.com>
2017-07-16 14:50:21 -0700

    subtract two numbers

---
+++ SubtractTwoNumbers.cs
@@ -0,0 +1,23 @@
+using System;
+
+class AddTwoNumbers
+{
+  static void Main()
+  {
+    Console.WriteLine("==========Subtraction==========");
+    Console.WriteLine("Give me two numbers to Subtract together.");
+    Console.WriteLine("First number is: ");
+    string stringFirstNumberSubtract = Console.ReadLine();
+    Console.WriteLine("Second number is: ");
+    string stringSecondNumberSubtract = Console.ReadLine();
+
+    int yourFirstNumberSubtract = int.Parse(stringFirstNumberSubtract);
+    int yourSecondNumberSubtract = int.Parse(stringSecondNumberSubtract);
+    int yourDifferenceNumber = yourFirstNumberSubtract - yourSecondNumberSubtract;
+
+    string stringDifferenceNumber = yourDifferenceNumber.ToString();
+
+    Console.WriteLine("When you add " + stringFirstNumberSubtract + " to " + stringSecondNumberSubtract + ".........");
+    Console.WriteLine("You get: " + stringDifferenceNumber + "\n");
+  }
+}
