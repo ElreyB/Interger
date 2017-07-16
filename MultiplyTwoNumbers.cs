a21d7c95f9949d5cc3720644b884cb197af6668e
ElreyB <elbelmonti@gmail.com>
2017-07-16 15:15:43 -0700

    WIP parsing the string to int

---
+++ MultiplyTwoNumbers.cs
@@ -0,0 +1,16 @@
+using System;
+
+class MultiplyTwoNumbers
+{
+  static void Main()
+  {
+    Console.WriteLine("==========Multiplication==========");
+    Console.WriteLine("Give me two numbers to multiply.");
+    Console.WriteLine("First number is: ");
+    string stringFirstNumberMultiply = Console.ReadLine();
+    Console.WriteLine("Second number is: ");
+    string stringSecondNumberMultiply = Console.ReadLine();
+
+    int
+  }
+}
