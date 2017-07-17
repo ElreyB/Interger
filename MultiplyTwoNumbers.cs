7b26132f78941ef229d60142370a3f97a9673a20
ElreyB <elbelmonti@gmail.com>
2017-07-16 17:07:49 -0700

    multiply two numbers together

--- MultiplyTwoNumbers.cs
+++ MultiplyTwoNumbers.cs
@@ -11,6 +11,13 @@ class MultiplyTwoNumbers
     Console.WriteLine("Second number is: ");
     string stringSecondNumberMultiply = Console.ReadLine();
 
-    int
+    int intFirstNumberMultiply = int.Parse(stringFirstNumberMultiply);
+    int intSecondNumberMultiply = int.Parse(stringSecondNumberMultiply);
+    int intFactorNumber = intFirstNumberMultiply * intSecondNumberMultiply;
+
+    string stringFactorNumber = intFactorNumber.ToString();
+
+    Console.WriteLine("When you multiply " + stringFirstNumberMultiply + " to " + stringSecondNumberMultiply + ".........");
+    Console.WriteLine("You get: " + stringFactorNumber);
   }
 }
